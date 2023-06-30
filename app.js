c4ed97863289388003e7ac00a5988fe02d389bdd
dev <devaggarwal190@gmail.com>
2023-06-30 14:03:05 +0530

    Final

---
+++ app.js
@@ -0,0 +1,16 @@
+const adviceNumber = document.getElementById('advice-number')
+const advice = document.getElementById('advice')
+const btn = document.getElementById('btn')
+
+async function getAdvice() {
+
+    const res = await fetch('https://api.adviceslip.com/advice')
+    const data = await res.json()
+    const randomAdvice = data.slip
+
+    adviceNumber.innerText = randomAdvice.id
+    advice.innerText = `"${randomAdvice.advice}"`
+
+}
+
+btn.addEventListener('click', getAdvice)
